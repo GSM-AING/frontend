b072cb8d52da014dbdf0e8720c2c9191ea89bb65
Jihyuk Lee <lead@sver.dev>
2024-04-07 23:00:25 +0900

    :construction: :: 홈 작업중 및 페이지 언어 변경

--- src/Pages/Main/index.tsx
+++ src/Pages/Main/index.tsx
@@ -1,5 +1,25 @@
 import * as S from "./style.ts";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <S.CenterObject>
+        <S.CenterText>당신을 위한 심리상담 플랫폼</S.CenterText>
+        <S.CenterMainText>먀옹</S.CenterMainText>
+        <S.CenterButtonObject>
+          <S.CenterButton>시작하기</S.CenterButton>
+          <S.CenterTextButton>로그인하기</S.CenterTextButton>
+        </S.CenterButtonObject>
+      </S.CenterObject>
+      <S.SecondObject>
+        <S.SecondMainText>
+          오늘의
+          <br />
+          기분은
+          <br />
+          어떠신가요?
+        </S.SecondMainText>
+      </S.SecondObject>
+    </div>
+  );
 }
--- src/Pages/Main/style.ts
+++ src/Pages/Main/style.ts
@@ -1,13 +1,55 @@
 import styled from "styled-components";
 
-export const Header = styled.div`
-  margin-left: auto;
-  margin-right: auto;
+export const CenterObject = styled.div`
   display: flex;
-  max-width: 80rem;
+  flex-direction: column;
   align-items: center;
-  justify-content: space-between;
-  padding: 1.25rem;
-  padding-left: 2rem;
-  padding-right: 2rem;
+  margin-top: 200px;
+  gap: 10px;
+`;
+
+export const CenterText = styled.div`
+  font-weight: 500;
+`;
+export const CenterMainText = styled.div`
+  font-size: 64px;
+  font-weight: 800;
+`;
+
+export const CenterButtonObject = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 40px;
+`;
+
+export const CenterButton = styled.div`
+  width: 100px;
+  height: 40px;
+  flex-shrink: 0;
+  border-radius: 20px;
+  background: #d9d9d9;
+  text-align: center;
+  justify-items: center;
+  line-height: 40px;
+`;
+
+export const CenterTextButton = styled.div`
+  color: #000;
+  text-align: center;
+  line-height: 40px;
+`;
+
+export const SecondObject = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 500px;
+`;
+
+export const SecondMainText = styled.div`
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 700;
+  white-space: pre-wrap;
+  line-height: normal;
 `;
