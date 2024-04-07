98f11dc42ca39ca3a580ce7e821bb198a3886d13
Jihyuk Lee <lead@sver.dev>
2024-04-07 21:46:53 +0900

    ✨ :: 상단바 제작

--- src/Pages/Main/index.tsx
+++ src/Pages/Main/index.tsx
@@ -1,10 +1,5 @@
 import * as S from "./style.ts";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-      <p>가장 먼저 보여지는 페이지입니다.</p>
-    </div>
-  );
+  return <div></div>;
 }
--- src/components/Topbar/style.ts
+++ src/components/Topbar/style.ts
@@ -4,10 +4,25 @@ export const Header = styled.div`
   margin-left: auto;
   margin-right: auto;
   display: flex;
-  max-width: 80rem;
+  white-space: nowrap;
   align-items: center;
   justify-content: space-between;
-  padding: 1.25rem;
   padding-left: 2rem;
   padding-right: 2rem;
+`;
+
+export const TextMenu = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 51px;
+  @media screen and (max-width: 375px) {
+    display: none;
+  }
+`;
+
+export const ImageMenu = styled.div`
+  display: none;
+  @media screen and (max-width: 375px) {
+    display: block;
+  }
 `;
